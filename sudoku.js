c0f265e6c8de86bfdaf157514b9f784c75ad868f
trevoribell <73569425+trevoribell@users.noreply.github.com>
2020-10-29 11:37:41 +1000

    Add files via upload

--- sudoku.js
+++ sudoku.js
@@ -2,7 +2,7 @@ Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + 
 
 function isNumberKey(evt) {
     var charCode = (evt.which) ? evt.which : event.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
+    if (charCode > 31 && (charCode < 49 || charCode > 57))
         return false;    
     return true;
 }
